d99d2decffa1177a9c3d056c0e7f7c81d82cbd45
Juan Carlos H. V <juankarlos.0304@gmail.com>
2018-07-05 09:40:07 -0500

    Se remplazo pymysql por mysqldb

--- src/DB_Manager.py
+++ src/DB_Manager.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys, pymysql
-pymysql.install_as_MySQLdb()
+import sys, MySQLdb
 
 class DB_Manager():
 	""" Class DB_Manager
@@ -24,7 +23,7 @@ class DB_Manager():
 		try:
 			datos = [self.DB_HOST, self.DB_USER, self.DB_PASS, self.DB_NAME]
 			#conn = MySQLdb.connect(*datos)
-			conn = pymysql.connect(*datos)
+			conn = MySQLdb.connect(*datos)
 			cursor = conn.cursor()
 			cursor.execute(query)
 			if query.upper().startswith('SELECT'):
